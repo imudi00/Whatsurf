679aa275843aecd4f9301d78cd67b2d1852d0a60
jinseo <realseo7668@gmail.comgit congif --global user.name>
2026-03-11 18:33:27 +0900

    refactor(AI): 기존 테스트 구조 제거 및 AI_test2 구조로 통합

--- AI/ai_clustering_test/src/clustering/embedding.py
+++
@@ -1,8 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-model = SentenceTransformer("snunlp/KR-SBERT-V40K-klueNLI-augSTS")
-
-
-def sbert_embedding(texts):
-    embeddings = model.encode(texts, batch_size=32, show_progress_bar=True)
-    return embeddings
--- AI/ai_clustering_test/src/clustering/tfidf_embedding.py
+++
@@ -1,9 +0,0 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-
-
-def tfidf_embedding(texts):
-    vectorizer = TfidfVectorizer(
-        max_features=10000
-    )
-    embeddings = vectorizer.fit_transform(texts)
-    return embeddings
